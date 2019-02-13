82f4cfe78965b63e0b832edd7eb07732237b8108
aelita <aelita.sh9@gmail.com>
2019-02-13 14:15:09 +0300

    words correction given one previous

--- spell_checker.py
+++ spell_checker.py
@@ -1,26 +1,81 @@
+import operator
 import re
 from collections import Counter
+from functools import reduce
 
 
 def words(text): return re.findall(r'\w+', text.lower())
 
 
+def words_n_nums(text): return re.findall(r'\w+|\d+', text.lower())
+
+
+def triples(words):
+    triples = {}
+    i = 0
+    while i in range(len(words) - 2):
+        triples[words[i] + ' ' + words[i + 1]] = int(words[i + 2])
+        i += 3
+    return triples
+
+
 WORDS = Counter(words(open('big.txt').read()))
+PHRASES = triples(words_n_nums(open('count_2w.txt').read()))
 
 
-def P(word, N=sum(WORDS.values())):
+def Pword(word, N=sum(WORDS.values())):
     """Probability of `word`."""
     return WORDS[word] / N
 
 
-def correction(word):
+def Pphrase(word, before=None, M=sum(PHRASES.values())):
+    if before:
+        try:
+            # print(f'----word={word} before={before}----')
+            return PHRASES[before + ' ' + word] / M
+        except Exception:
+            print(f'-------word = {word} before = {before}-------')
+
+            return 1 / M
+
+    return Pword(word)
+
+
+def text_correction(text):
+    "Spell-correct all words in text."
+    result = ''
+    tokens = words(text)
+    for i in range(len(tokens)):
+        if i > 0 and known(tokens[i - 1]):
+            result += ' ' + phrase_correction(before=tokens[i - 1], word=tokens[i])
+        else:
+            result += ' ' + phrase_correction(word=tokens[i])
+    return result
+
+
+def word_correction(word):
     """Most probable spelling correction for word."""
-    return max(candidates(word), key=P)
+    return max(candidates(word), key=Pword)
+
+
+def phrase_correction(word, before=None):
+    max = -1
+    result = ''
+    c = candidates(word)
+    for item in c:
+        p = Pphrase(item, before)
+        print(f'before = {before}, word = {item}, p = {p}, max = {max}')
+        if p and p > max:
+            max = p
+            result = item
+            print(f'UPDATED: before = {before}, word = {item}, p = {p}, max = {max}')
+            print("")
+    return result
 
 
 def candidates(word):
     """Generate possible spelling corrections for word."""
-    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+    return known([word]) or known(edits1(word)) or known(edits2(word)) or [word]
 
 
 def known(words):
@@ -30,7 +85,7 @@ def known(words):
 
 def edits1(word):
     """All edits that are one edit away from `word`."""
-    letters = 'abcdefghijklmnopqrstuvwxyz '
+    letters = 'abcdefghijklmnopqrstuvwxyz'
     splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
     deletes = [L + R[1:] for L, R in splits if R]
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
@@ -44,9 +99,30 @@ def edits2(word):
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
 
-def trail(word, length):
-    if len(word) > length:
-        return word[0:length]
-    while len(word) < length:
-        word += '0'
-    return word
+def segment(text):
+    "Return a list of words that is the best segmentation of text."
+    if not text: return []
+    candidates = ([first] + segment(rem) for first, rem in splits(text))
+    return max(candidates, key=Pwords)
+
+
+def splits(text, L=20):
+    "Return a list of all possible (first, rem) pairs, len(first)<=L."
+    return [(text[:i + 1], text[i + 1:])
+            for i in range(min(len(text), L))]
+
+
+def Pwords(words):
+    "The Naive Bayes probability of a sequence of words."
+    return product(Pword(word) for word in words)
+
+
+def product(nums):
+    "Return the product of a sequence of numbers."
+    return reduce(operator.mul, nums, 1)
+
+
+# print(text_correction("London ia a caapitak"))
+print(Pphrase("is", "london"))
+print(Pphrase("in", "london"))
+print(Pphrase("a", "london"))
